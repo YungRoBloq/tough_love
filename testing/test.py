99b88fab07508031cef3c1ddb99d5e950530716d
Jorge Ortega <Jorge.ortega@chubb.com>
2025-09-28 04:39:12 -0400

    Finalize Frontend and API

---
+++ testing/test.py
@@ -0,0 +1,14 @@
+#API To Receive Data
+from fastapi import FastAPI, Request
+import uvicorn 
+
+app = FastAPI()
+
+@app.post("/data")
+async def receive_data(request: Request):
+    data = await request.json()
+    return {"status": "success", 
+            "received": data}
+
+if __name__ == "__main__":
+    uvicorn.run("test:app", host="0.0.0.0", port=8000, reload=True)
